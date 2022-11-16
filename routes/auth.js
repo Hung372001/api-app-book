3a8cc6064ad7965f135ea80756fcc4fbb3a0657a
hung372001 <ocoten321@gmail.com>
2022-11-17 02:10:31 +0700

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+
 
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
+  
 
     const newUser = new User({
       username: req.body.username,
